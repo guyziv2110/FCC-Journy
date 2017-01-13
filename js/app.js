ef7545d9c85536b5297e49ee550d891970c9b4d5
guy <guyziv2110@gmail.com>
2017-01-13 14:14:56 +0200

    added quotes buffering

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-var lastQuote = "I don't know why we are here, but I'm pretty sure that it is not in order to enjoy ourselves";
-var lastAuthor = "Ludwig Wittgenstein";
+var quotes = [
+  {quote: "I don't know why we are here, but I'm pretty sure that it is not in order to enjoy ourselves",
+   author: "Ludwig Wittgenstein"}
+];
 
-function getQuote() {
+function loadQuote() {
   $.ajax({
     headers:{
       "X-Mashape-Key":'FqijuuCXU5mshfIAleZAkKqbqFrnp1ruFFujsn2osuYKELy3Lu'
@@ -9,22 +11,36 @@ function getQuote() {
     url:'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
     success: function(response) {
       var r = JSON.parse(response);
-      lastQuote = r.quote;
-      lastAuthor = r.author;
-      setQuoteElement(".quote-text", r.quote);
-      setQuoteElement(".quote-author", r.author);
+      quotes.push({quote: r.quote, author: r.author});
     }
   });
 }
 
+function newQuoteRequest() {
+    quotes.shift();
+    loadQuote();  
+    setQuote();  
+}
+
+
+function setQuote() {
+    var currentQuote = quotes[0];
+    setQuoteElement(".quote-text", currentQuote.quote);
+    setQuoteElement(".quote-author", currentQuote.author);    
+}
+
 function tweetQuoteInfo() {
-  $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + lastQuote + '" -' + lastAuthor));
+  $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' 
+              + encodeURIComponent('"' + quotes[0].quote + '" -' + quotes[0].author));
 }
 
 $(document).ready(function() {
-  $('#new-quote').click(getQuote);
+  setQuote();
+  loadQuote();
+  $('#new-quote').click(newQuoteRequest);
   $('#tweet-quote').click(tweetQuoteInfo);
 });
+
 
 function setQuoteElement(quoteText, quote) {
   $(quoteText).animate({
