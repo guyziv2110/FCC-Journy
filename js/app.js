998e7fd4944d6ff46d0b8f8f7d1f216d6485c558
guy <guyziv2110@gmail.com>
2017-01-13 16:41:44 +0200

    added validation/synchronized actions

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 var quotes = [
   {quote: "When you do the common things in life in an uncommon way, you will command the attention of the world.",
-   author: "Joe Turnham"}
+   author: "Joe Turnham"},
+  {quote:"I have come to believe that the whole world is an enigma, a harmless enigma that is made terrible by our own mad attempt to interpret it as though it had an underlying truth.",
+    author:"Umberto Eco"}
 ];
 
-function loadQuote() {
+function loadQuote(callback) {
   $.ajax({
     headers:{
       "X-Mashape-Key":'FqijuuCXU5mshfIAleZAkKqbqFrnp1ruFFujsn2osuYKELy3Lu'
@@ -12,6 +14,7 @@ function loadQuote() {
     success: function(response) {
       var r = JSON.parse(response);
       quotes.push({quote: r.quote, author: r.author});
+      callback();
     }
   });
 }
@@ -19,8 +22,13 @@ function loadQuote() {
 function newQuoteRequest(e) {
     e.preventDefault();
     quotes.shift();
-    loadQuote();  
-    setQuote();  
+
+    if(quotes[0] !== undefined) {
+      setQuote();  
+    }
+
+    $("#new-quote").addClass("disabled");
+    loadQuote(function() {$("#new-quote").removeClass("disabled")});
 }
 
 function setQuote() {
@@ -30,8 +38,10 @@ function setQuote() {
 }
 
 function tweetQuoteInfo() {
-  $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' 
-              + encodeURIComponent('"' + quotes[0].quote + '" -' + quotes[0].author));
+  if(quotes[0] !== undefined) {
+    $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' 
+                + encodeURIComponent('"' + quotes[0].quote + '" -' + quotes[0].author));
+  }
 }
 
 function setQuoteElement(quoteText, quote) {
