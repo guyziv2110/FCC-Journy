bcd4f7c3ea621adad0ee2c6daa8a1a399747ec78
guy <guyziv2110@gmail.com>
2017-01-23 09:01:14 +0200

    added ui/functionality

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
 function getWikipediaArticles() {
-var apiBase = "https://en.wikipedia.org/w/api.php" + 
-                                    "?format=json" +
-                                    "&action=query" +
-                                    "&generator=search" +
-                                    "&gsrnamespace=0" + // default (no namespace)
-                                    "&gsrlimit=5" +
-                                    "&prop=pageimages|extracts" +
-                                    "&pilimit=max" // page images generated;
-var format = "json";
-var action = "query";
-var generator = "search";
+    //https://www.mediawiki.org/wiki/Extension:TextExtracts
+    var searchString = "A";
+    var apiSearchURL = "https://en.wikipedia.org/w/api.php" + 
+                                        "?format=json" +
+                                        "&action=query" +
+                                        "&generator=search" +
+                                        "&gsrnamespace=0" + // default (no namespace)
+                                        "&gsrlimit=10" +
+                                        "&prop=pageimages|extracts" +
+                                        "&pilimit=max" + // page images generated
+                                        "&exintro" + // returns only content before the first section
+                                        "&explaintext" + // returns plaintext instead of HTML
+                                        "&exsentences=1" +
+                                        "&exlimit=max" +
+                                        "&gsrsearch=" + searchString; 
+    console.log(apiSearchURL);
 
 }
+
+getWikipediaArticles();
