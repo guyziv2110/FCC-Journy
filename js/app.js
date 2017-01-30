96bf9a2b93051ef131da5b22ac7f361d625b4c5b
guy <guyziv2110@gmail.com>
2017-01-30 09:14:00 +0200

    investigating twitch tv api

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
 /* client id = e6chpbil9u09n2xi2yg8uj5mxzmum8 */
 /* featured link = https://api.twitch.tv/kraken/streams/featured?limit=5&client_id=e6chpbil9u09n2xi2yg8uj5mxzmum8 */
+
+
+/* channel info = 
+without stream:
+'https://api.twitch.tv/kraken/streams/shoryuken_this/?client_id=e6chpbil9u09n2xi2yg8uj5mxzmum8
+
+with stream:
+https://api.twitch.tv/kraken/streams/esl_sc2/?client_id=e6chpbil9u09n2xi2yg8uj5mxzmum8
+*/
+
