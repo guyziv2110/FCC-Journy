47f98f40166a8516e94a87c5df467b91ccb323ee
guy <guyziv2110@gmail.com>
2017-01-31 08:23:50 +0200

    added channel panel

--- js/app.js
+++ js/app.js
@@ -10,3 +10,14 @@ with stream:
 https://api.twitch.tv/kraken/streams/esl_sc2/?client_id=e6chpbil9u09n2xi2yg8uj5mxzmum8
 */
 
+function resizeHandler(windowWidth) {
+    if (windowWidth > 850) {
+        $('.channels-panel').removeClass('closed');
+    } else if (windowWidth <= 850) {
+        $('.channels-panel').addClass('closed');
+    }
+}
+
+$(window).resize(function() {
+    resizeHandler($(window).width());
+});
