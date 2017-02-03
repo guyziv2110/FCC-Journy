032f477d2f4652882a1fe571e61fc9aba46f8981
guy <guyziv2110@gmail.com>
2017-02-03 17:25:26 +0200

    final functionality/design changes

--- js/app.js
+++ js/app.js
@@ -26,10 +26,12 @@ if (!String.format) {
 }
 
 function resizeHandler(windowWidth) {
-    if (windowWidth > 850) {
+    if (windowWidth > 1040) {
         $('.channels-panel').removeClass('closed');
-    } else if (windowWidth <= 850) {
+        $('.channel-featured-content').removeClass('full');
+    } else if (windowWidth <= 1040) {
         $('.channels-panel').addClass('closed');
+        $('.channel-featured-content').addClass('full');
     }
 }
 
@@ -38,6 +40,7 @@ $(window).resize(function() {
 });
 
 $(document).ready(function() {
+    resizeHandler($(window).width());
     populateFaveStreamersList();
     fetchFaveStreamersChannels();
 });
@@ -55,6 +58,7 @@ function fetchChannel(channel) {
     var fetchPromise = fetch(channelUrl);
 
     fetchPromise.done(function(res) {
+        console.log(res);
         parseApiResponse(channel, res);
     });
 }
@@ -65,15 +69,51 @@ function fetch(channelUrl) {
     });
 }
 
+function buildFaveStreamersBoxes(channel, res) {
+    var appended = String.format("\
+    <div class='fav-box' id='fav-box-{0}'> \
+        <div class='fav-box-preview-logo'> \
+            <a id='link-{0}' href='{1}' target='_blank'> \
+                <img src='{2}'></img> \
+            </a>\
+        </div> \
+        <div class='fav-box-information'> \
+            <div class='fav-box-channel-name'>{9}</div>\
+            <div class='fav-box-game-name'>{3}</div>\
+            <div class='fav-status'>{4}</div>\
+            <hr class='additional-info-sep'> \
+            <div class='additional-info'> \
+                <div>{5}p, {6}</div>\
+                <div>{7} fps</div>\
+                <div>{8} viewers</div>\
+            <\div> \
+        </div> \
+    </div>", channel, 
+             "https://www.twitch.tv/" + channel, 
+             res.stream.preview.large,
+             res.stream.channel.game,
+             res.stream.channel.status,
+             res.stream.video_height,
+             res.stream.channel.language,
+             Math.round(res.stream.average_fps),
+             res.stream.viewers,
+             res.stream.channel.name );
+
+    $('#fav-streamers-boxes-information').append(appended);
+}
+
 function parseApiResponse(channel, res) {
     if(res === undefined || res === null) {
         $('#fav-streamer-' + channel).addClass('not-found');
     }
     else if(res.stream === null) {
         $('#fav-streamer-' + channel).addClass('offline');
+        $('#link-' + channel).attr('href', "https://www.twitch.tv/" + channel);
     }
     else {
         $('#fav-streamer-' + channel).addClass('online');
+        $('#link-' + channel).attr('href', "https://www.twitch.tv/" + channel);
+        buildFaveStreamersBoxes(channel, res);
     }
 }
 
@@ -87,11 +127,35 @@ function populateFaveStreamersList() {
 function appendFaveStreamerToList(channel) {
     var appended = String.format("\
     <li class='fav-streamer' id='fav-streamer-{0}'> \
-        <a class='link-{0}' href='#'>{0}</a>\
+        <a id='link-{0}' href='#' target='_blank'>{0}</a>\
         <span class='delete-fave-stream' id='delete-{0}'> \
             <i class='fa fa-trash-o' aria-hidden='true'></i> \
         </span> \
     </li>", channel);
 
     $('#fav-streamers-list').append(appended);
+    $('#delete-' + channel).click(function() {
+        removeChannelFromFaves(channel);
+    });
 }
+
+function removeChannelFromFaves(channel) {
+    var channelIndex = userFaveStreamersChannels.indexOf(channel);
+
+    if(channelIndex > -1) {
+        userFaveStreamersChannels.splice(channelIndex, 1);
+
+        $("#fav-box-" + channel).fadeOut(220, function(){
+            $(this).remove();
+        });
+
+        $("#fav-streamer-" + channel).fadeOut(220, function() {
+            $(this).remove();
+        });
+    }
+}
+
+$('#channel-panel-handle').click(function() {
+    $('.channels-panel').toggleClass('closed');
+    $('.channel-featured-content').toggleClass('full');
+});
