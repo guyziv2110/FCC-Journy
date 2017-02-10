e8b2621ababee0bff217efdc1324212ffe0d3f85
guy <guyziv2110@gmail.com>
2017-02-10 13:02:06 +0200

    added solution

--- Intermediate-Algorithm-Scripting/missing_letters/solution.js
+++ Intermediate-Algorithm-Scripting/missing_letters/solution.js
@@ -1,5 +1,16 @@
 function fearNotLetter(str) {
-  return str;
+  var missingChar;
+  var currentCharCode, nextCodeChar;
+
+  for(var i = 0; i < str.length; i++) {
+    currentCharCode = str.charCodeAt(i);
+    nextCodeChar = str.charCodeAt(0) + i;
+    
+    if(currentCharCode !== nextCodeChar) {
+      return String.fromCharCode(currentCharCode - 1);
+    }
+  }
+  return missingChar;
 }
 
 fearNotLetter("abce");
