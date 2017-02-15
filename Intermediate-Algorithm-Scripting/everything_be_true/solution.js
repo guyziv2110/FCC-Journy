179d342c44a938f9e8822fc0fa73c3a3c4f25695
guy <guyziv2110@gmail.com>
2017-02-15 08:07:21 +0200

    added solution

---
+++ Intermediate-Algorithm-Scripting/everything_be_true/solution.js
@@ -0,0 +1,8 @@
+function truthCheck(collection, pre) {
+  // Is everyone being true?
+  return collection.every((element) => {
+    return (element.hasOwnProperty(pre) && Boolean(element[pre]));
+  });
+}
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
