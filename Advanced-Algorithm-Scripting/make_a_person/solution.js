1b0d635d664e651ac008b6df89f839163a964f13
guy <guyziv2110@gmail.com>
2017-03-01 09:48:11 +0200

    completed solution

--- Advanced-Algorithm-Scripting/make_a_person/solution.js
+++ Advanced-Algorithm-Scripting/make_a_person/solution.js
@@ -1,5 +1,32 @@
 var Person = function(firstAndLast) {
-    return firstAndLast;
+    var firstName = firstAndLast.split(" ")[0];
+    var lastName = firstAndLast.split(" ")[1];
+
+    this.getFirstName = function() {
+        return firstName;
+    };
+
+    this.getLastName = function() {
+        return lastName;
+    };
+
+    this.getFullName = function() {
+        return firstName + " " + lastName;
+    };
+
+    this.setFirstName = function(first) {
+        firstName = first;
+    };
+
+    this.setLastName = function(last) {
+        lastName = last;
+    };
+
+    this.setFullName = function(firstAndLast) {
+        firstAndLastArr = firstAndLast.split(" ");
+        firstName = firstAndLastArr[0];
+        lastName = firstAndLastArr[1];
+    };  
 };
 
 var bob = new Person('Bob Ross');
