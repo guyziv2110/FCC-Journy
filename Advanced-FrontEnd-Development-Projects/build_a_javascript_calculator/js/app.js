11f695ded545ddf05185805c331429e8ae793c09
guy <guyziv2110@gmail.com>
2017-03-08 08:51:36 +0200

    added functionality to the calc manager(app)

--- Advanced-FrontEnd-Development-Projects/build_a_javascript_calculator/js/app.js
+++ Advanced-FrontEnd-Development-Projects/build_a_javascript_calculator/js/app.js
@@ -5,28 +5,52 @@ $(document).ready(function() {
 function appInit() {
     return function() {
         var c1 = new Calculator();
+        console.log(c1);
         var expression = [];
         var operators = [];
+        var exp = "";
         
+        var operatorFunc = {
+            '+': function(a, b) {return c1.add(a,  b);},
+            '-': function(a, b) {return c1.subtract(a, b);},
+            '*': function(a, b) {return c1.mul(a, b);},
+            '/': function(a, b) {return c1.div(a, b);}
+        };
 
         $('button').click(function(e) {
             var val = e.target.value;
 
             if(val === '=') {
+                if (exp) {
+                    expression.push(parseInt(exp)); 
+                    exp = "";
+                }
                 while (operators.length > 0) {
                     var op = operators.pop();
                     expression.push(op);
-                }                
-                console.log(expression);
+                }         
+
+                var res = calcPostfixExpression(expression);
+                console.log(res);
             }
             
             else if (isOperator(val)) {
-                var topOperator = operators.length > 1 ? operators[operators.length-1] : null;
+               
+                if (exp) {
+                    expression.push(parseInt(exp)); 
+                    exp = "";
+                }
+
+                var topOperator = operators.length > 0 ? operators[operators.length-1] : null;                   
                 if ((topOperator === '+' || topOperator === '-') &&
-                    val === '/' || val === '*')
+                    val === '/' || val === '*') {
                     operators.push(val);
-                else if (topOperator === null)
+                    console.log(operators);
+                }
+                else if (topOperator === null) {
                     operators.push(val);
+                    console.log(operators);
+                }
                 else {
                     while (operators.length > 0) {
                         var op = operators.pop();
@@ -34,11 +58,16 @@ function appInit() {
                     }
 
                     operators.push(val);
+
+
+
+                    console.log(operators);
+                    console.log(expression);                    
                 }
 
             }
             else if(isOperand(val)) {
-                expression.push(val);
+                exp += val;
             }
             
         });    
@@ -50,10 +79,32 @@ function appInit() {
         }
 
         function isOperand(v) {
-            var re = /^\d$/;
+            var re = /^\d+$/;
             return re.test(v);
         }
 
+        function calcPostfixExpression(exp) {
+            var k;
+            var a, b, c;
+            var expstack = [];
+
+            while (exp.length) {
+                k = exp.shift();
+                if (isOperand(k))
+                    expstack.push(k);
+                else {
+                    b = expstack.pop()
+                    a = expstack.pop();
+                    c = operatorFunc[k](a, b);
+                    expstack.push(c);
+                }
+            }
+
+            return expstack.pop();
+        }
+
+
+      
     }();
 }
    
--- Advanced-FrontEnd-Development-Projects/build_a_javascript_calculator/js/calculator.js
+++ Advanced-FrontEnd-Development-Projects/build_a_javascript_calculator/js/calculator.js
@@ -1,27 +1,22 @@
 
 var Calculator = (function () {
     var Calculator = function() {
-        this.result = 0;
-
-        this.getResult = function() {
-            return this.result;
-        }
     }
 
     Calculator.prototype.add = function(x, y) {
-        this.result = x + y;
+        return x + y;
     }
 
     Calculator.prototype.subtract = function(x, y) {
-        this.result = x - y;
+        return x - y;
     }
 
     Calculator.prototype.mul = function(x, y) {
-        this.result = x * y;
+        return x * y;
     }
 
     Calculator.prototype.div = function(x, y) {
-        this.result = x / y;
+        return x / y;
     }
 
     return Calculator;
