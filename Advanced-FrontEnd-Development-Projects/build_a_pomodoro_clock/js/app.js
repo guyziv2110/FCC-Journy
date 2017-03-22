724e525f8e5fb3e1f64b8b2a9386c991580ca4d2
guy <guyziv2110@gmail.com>
2017-03-22 08:24:09 +0200

    implmeneted pomodo manager

--- Advanced-FrontEnd-Development-Projects/build_a_pomodoro_clock/js/app.js
+++ Advanced-FrontEnd-Development-Projects/build_a_pomodoro_clock/js/app.js
@@ -15,25 +15,15 @@
             });
         });
 
+        var pomoManager = pomodoroManager();
 
-        var circle = document.getElementById('pomodoro_timer_circle_filler');
-        var interval = 30;
-        var angle = 0;
-        var angle_increment = 6;
-        var circle_radius = circle.getAttribute('r');
+        $('.pomodoro_timer_state').click(function() {
+            if(pomoManager.isStarted() === undefined)
+                pomoManager.start();
+            else
+                pomoManager.pause();
+        });
 
-        // circumference of a circle is : 2PI * R.
-        var full_angle = 2 * Math.PI * circle_radius;
-        window.timer = window.setInterval(function () {
-            circle.setAttribute("stroke", "#FF8D82");
-            circle.setAttribute("stroke-dasharray", angle + ", 200000");
-
-
-            if (angle >= full_angle) {
-                window.clearInterval(window.timer);
-            }
-            angle += angle_increment;
-        }.bind(this), interval);
 })(pomodoroManagerReference, pomodoroTypeEnum);
 
 // add pomodoro manager to organize stop timer, start timer and etc...
--- Advanced-FrontEnd-Development-Projects/build_a_pomodoro_clock/js/pomodoro_manager.js
+++ Advanced-FrontEnd-Development-Projects/build_a_pomodoro_clock/js/pomodoro_manager.js
@@ -1,4 +1,7 @@
 function pomodoroManager() {
+    var pomoTimer;
+    var lastAngle = 0;
+
     var setSession = function() {
         console.log('setSession called');
     }
@@ -7,13 +10,40 @@ function pomodoroManager() {
         console.log('setBreak called');
     }
 
-    var start = function() {
+    var pause = function() {
+         clearInterval(pomoTimer);
+         pomoTimer = undefined;
+    }
 
+    var start = function() {
+        var circle = document.getElementById('pomodoro_timer_circle_filler');
+        var interval = 90;
+        var angle_increment = 6;
+        var circle_radius = circle.getAttribute('r');
+
+        // circumference of a circle is : 2PI * R.
+        var full_angle = 2 * Math.PI * circle_radius;        
+        pomoTimer = window.setInterval(function () {
+            circle.setAttribute("stroke", "#FF8D82");
+            circle.setAttribute("stroke-dasharray", lastAngle + ", 200000");
+
+
+            if (lastAngle >= full_angle) {
+                window.clearInterval(pomoTimer);
+            }
+            lastAngle += angle_increment;
+        }.bind(this), interval);
+    }
+
+    var isStarted = function() {
+        return pomoTimer;
     }
 
     return {
         setSession: setSession,
         setBreak: setBreak,
+        pause: pause,
+        isStarted: isStarted,
         start: start
     }
 }
