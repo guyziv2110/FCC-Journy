10539ab036382df4608f8d05a50279ebfa6a6bd2
guy <guyziv2110@gmail.com>
2017-04-01 20:37:54 +0300

    working on game board design

--- Advanced-FrontEnd-Development-Projects/build_a_tictactoe_game/js/GameManager.js
+++ Advanced-FrontEnd-Development-Projects/build_a_tictactoe_game/js/GameManager.js
@@ -1,7 +1,7 @@
 // controller
 function GameManager() {
     var gameBoard = new GameBoard(); // GameBoard instance
-
+    // working on game board design
      
     // methods 
     // access GameManagerUI to paint the board.
