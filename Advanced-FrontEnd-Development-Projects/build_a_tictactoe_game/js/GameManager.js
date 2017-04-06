46ee992cfbc11daa447ed14a3bb1871d09ceaecb
guy <guyziv2110@gmail.com>
2017-04-06 09:12:21 +0300

    small logic add

--- Advanced-FrontEnd-Development-Projects/build_a_tictactoe_game/js/GameManager.js
+++ Advanced-FrontEnd-Development-Projects/build_a_tictactoe_game/js/GameManager.js
@@ -3,24 +3,29 @@ function GameManager() {
     var gameBoard = new GameBoard(); // GameBoard instance
     var gameManager = GameManagerUI(gameBoard.board);
     var humanPlayer = new HumanPlayer();
-    var plType;
+    var humanDrawType;
+    var computerDrawType;
     // working on game board design
      
     // methods 
     // access GameManagerUI to paint the board.
     // 
 
-    this.start = function(playerType) {
-        plType = playerType;
-        gameManager.draw(playerType);
+    this.start = function(humDrawType) {
+        humanDrawType = humDrawType;
+        gameManager.draw(humanDrawType);
         gameplay();
+    }
+
+    var getComputerDrawType = function() {
+        return humanDrawType === 'X' ? 'O' : 'X';
     }
 
     var gameplay = function() {
         var promise = humanPlayer.playTurn();
         promise.then(function(res) {
             var rowcol = res.replace('cell', '');
-            gameBoard.setItem(rowcol[0], rowcol[1], plType);
+            gameBoard.setItem(rowcol[0], rowcol[1], humanDrawType);
             gameplay();
         });
     }
