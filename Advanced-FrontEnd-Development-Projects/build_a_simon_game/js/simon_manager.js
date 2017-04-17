775aa24d2538d0b36c4d3075592ef405ea9e7264
guy <guyziv2110@gmail.com>
2017-04-17 11:56:34 +0300

    simon logic / ui notes

---
+++ Advanced-FrontEnd-Development-Projects/build_a_simon_game/js/simon_manager.js
@@ -0,0 +1,13 @@
+function SimonManager() {
+    // logic
+    // 1. generate random number between 0 to 3 (indicating the next random color to be blinked)
+    // 2. save the generated random number in an array (to keep the whole series of blinking)
+
+    // UI
+    // 3. when a random number is generated show blinking (lock clicks and etc...)
+    
+
+    // notes:
+    // support strict mode (after one mistake game restart)
+    // support count displaying, game starting and so on...
+}
